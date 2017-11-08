a055ece7aeb9b71260651b2335d477daa63b56fc
Daniel Crenna <daniel.crenna@gmail.com>
2017-11-07 21:58:44 -0500

    hoist IActor/IAudioPlayer

---
+++ src/Pixel3D.Audio/IAudioPlayer.cs
@@ -0,0 +1,16 @@
+﻿using Pixel3D.Audio;
+using Pixel3D.Engine;
+
+namespace RCRU.Engine
+{
+    public interface IAudioPlayer
+    {
+        /// <summary>Play a sound without any position (always plays centred)</summary>
+        void PlayCueGlobal(string symbol, IActor source = null); // <- keeping source around, in case it is useful information (will become useful for rollback)
+        
+        /// <summary>Play a sound without any position (always plays centred)</summary>
+        void PlayCueGlobal(Cue cue, IActor source = null); // <- keeping source around, in case it is useful information (will become useful for rollback)
+
+        void PlayMenuMusic(string symbol, bool loop = true, bool synchronise = false);
+    }
+}
