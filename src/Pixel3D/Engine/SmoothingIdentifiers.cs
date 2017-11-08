f890ca4b2663f3c60440c9df765f1d07ebe1f9b3
Daniel Crenna <daniel.crenna@gmail.com>
2017-11-08 02:17:48 -0500

    add SmoothingIdentifiers

---
+++ src/Pixel3D/Engine/SmoothingIdentifiers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Pixel3D.Engine
+{
+    public struct SmoothingIdentifiers
+    {
+        // Don't need equality check methods because SmoothingManager does the right thing. (And no one will break it, right?)
+        public Type type;
+        public int id;
+    }
+}
