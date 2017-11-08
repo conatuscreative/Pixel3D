d2d82969a0a04f6d2ada3c9b15400ee1e96119f3
Daniel Crenna <daniel.crenna@gmail.com>
2017-11-08 16:14:15 -0500

    stub out UpdateContext

--- src/Pixel3D/Engine/UpdateContext.cs
+++ src/Pixel3D/Engine/UpdateContext.cs
@@ -4,9 +4,36 @@ namespace Pixel3D.Engine
 {
     public class UpdateContext : IUpdateContext
     {
+        protected readonly SoundRollbackManager soundRollbackManager;
+
         public UpdateContext(Camera audioCamera, SoundRollbackManager soundRollbackManager)
+        {
+            this.soundRollbackManager = soundRollbackManager;
+            AudioCamera = audioCamera;
+        }
+
+        /// <summary>
+        /// The context must not retain data across frames, because it is not part of the networked state.
+        /// So, for safety, reset it every time it is used.
+        /// </summary>
+        public virtual void Reset()
         {
 
         }
+
+        #region Non-Retained Data
+
+        // Things that must not be retained across frames (clear in Reset method!)
+
+
+        #endregion
+
+        #region Audio
+
+        /// <summary>For positioning audio in world space. May be null.</summary>
+        protected Camera AudioCamera { get; set; }
+
+
+        #endregion
     }
 }
