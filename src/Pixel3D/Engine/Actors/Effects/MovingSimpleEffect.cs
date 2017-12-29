8d6ed7757f66138e962a41cefe7bc4d12dbfa4e6
Daniel Crenna <daniel@hq.io>
2017-12-29 13:46:52 -0500

    hoist base effect classes

---
+++ src/Pixel3D/Engine/Actors/Effects/MovingSimpleEffect.cs
@@ -0,0 +1,24 @@
+﻿using Pixel3D.Animations;
+
+namespace Pixel3D.Engine.Actors.Effects
+{
+    /// <summary>This exists primaraly so I don't have to edit animations to remove gameplay motion. Best avoid using deliberately.</summary>
+    public class MovingSimpleEffect : SimpleEffect
+    {
+        public MovingSimpleEffect(AnimationSet animationSet, UpdateContext updateContext, string animationTag, Position position, bool facingLeft)
+                : base(animationSet, updateContext, animationTag, position, facingLeft)
+        {
+        }
+
+
+        public override void Update(UpdateContext updateContext)
+        {
+            base.Update(updateContext);
+
+            var positionDelta = currentAnimation.PositionDeltaThisTick();
+            if(facingLeft)
+                positionDelta.X = -positionDelta.X;
+            position += positionDelta;
+        }
+    }
+}
