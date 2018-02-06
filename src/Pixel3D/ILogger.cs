7aa652699e386b6fd009f40f21276aacaad2c602
Daniel Crenna <daniel@hq.io>
2018-02-06 11:58:14 -0500

    this interface is really bad, but i'll clean it up later

--- src/Pixel3D/ILogger.cs
+++ src/Pixel3D/ILogger.cs
@@ -13,11 +13,12 @@ namespace Pixel3D
         
         void Error(string message);
         void Error(string message, Exception exception);
-        void ErrorException(string message, Exception exception);
-        void FatalException(string message, Exception exception);
+        void ErrorException(string message, Exception exception, params object[] args);
         
+        
+        void Fatal(string message, params object[] args);
+        void FatalException(string message, Exception exception, params object[] args);
         void Fatal(Exception exception);
-        void Fatal(string message);
         void Fatal(string message, Exception exception);
 
         void WarnException(string message, Exception exception);
