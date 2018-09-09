d4fd38f4e58f2466e46cf0e4abe85aed6d9d7e5b
Daniel Crenna <daniel@hq.io>
2018-09-09 16:02:41 -0400

    binary extensions are not primitives

--- src/Pixel3D/Extensions/PositionExtensions.cs
+++ src/Pixel3D/Extensions/PositionExtensions.cs
@@ -1,9 +1,26 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.IO;
+using Microsoft.Xna.Framework;
 
 namespace Pixel3D
 {
 	public static class PositionExtensions
 	{
+		public static void Write(this BinaryWriter bw, Position position)
+		{
+			bw.Write(position.X);
+			bw.Write(position.Y);
+			bw.Write(position.Z);
+		}
+
+		public static Position ReadPosition(this BinaryReader br)
+		{
+			Position p;
+			p.X = br.ReadInt32();
+			p.Y = br.ReadInt32();
+			p.Z = br.ReadInt32();
+			return p;
+		}
+
 		public static Point GetXY(this Position position)
 		{
 			return new Point(position.X, position.Y);
