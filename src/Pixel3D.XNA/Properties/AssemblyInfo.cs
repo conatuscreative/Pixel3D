4643081d91e7bafbf1accb962d26ce3113556534
Daniel Crenna <daniel@hq.io>
2018-09-09 23:49:58 -0400

    create Pixel3D XNA compatibility library

---
+++ src/Pixel3D.XNA/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pixel3D.XNA")]
+[assembly: AssemblyProduct("Pixel3D.XNA")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Conatus Creative Inc.")]
+[assembly: AssemblyCopyright("Copyright © 2018 Conatus Creative Inc.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("f7258856-9234-4523-a8a9-2fc3230d9906")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
