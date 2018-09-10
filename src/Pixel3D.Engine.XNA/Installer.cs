87018f012aa353300e7a2487f808eb58646ce908
Daniel Crenna <daniel@hq.io>
2018-09-10 00:31:38 -0400

    cleanup XNA installer

--- src/Pixel3D.Engine.XNA/Installer.cs
+++ src/Pixel3D.Engine.XNA/Installer.cs
@@ -1,170 +1,173 @@
-﻿//using System;
-//using System.Diagnostics;
-//using System.IO;
-//using Microsoft.Xna.Framework.Audio;
-//using Microsoft.Xna.Framework.Graphics;
-//using Pixel3D.Animations;
-//using Pixel3D.AssetManagement;
-//using Pixel3D.Audio;
-//using Pixel3D.Engine.Audio;
-//using Pixel3D.LoopRecorder;
-//using Pixel3D.Serialization;
-//using Pixel3D.Serialization.Context;
-//using SoundState = Pixel3D.Audio.SoundState;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Pixel3D.Animations;
+using Pixel3D.AssetManagement;
+using Pixel3D.Audio;
+using Pixel3D.LoopRecorder;
+using Pixel3D.Serialization;
+using Pixel3D.Serialization.Context;
+using SoundState = Pixel3D.Audio.SoundState;
 
-//namespace Pixel3D.Engine.FNA
-//{
-//    /// <summary>
-//    /// Links independent Pixel3D engine components to XNA. Should be called once from an external game project.
-//    /// </summary>
-//    public static class Installer
-//    {
-//        public static void Install<TGameState>(int maxPlayers)
-//        {
-//            AssetReader.serviceObjectProvider = services =>
-//                ((IGraphicsDeviceService)services.GetService(typeof(IGraphicsDeviceService))).GraphicsDevice;
+namespace Pixel3D.Engine.XNA
+{
+    /// <summary>
+    /// Links independent Pixel3D engine components to XNA. Should be called once from an external game project.
+    /// </summary>
+    public static class Installer
+    {
+        public static void Install<TGameState>(int maxPlayers)
+        {
+            AssetReader.serviceObjectProvider = services =>
+                ((IGraphicsDeviceService)services.GetService(typeof(IGraphicsDeviceService))).GraphicsDevice;
 
-//            InstallAudioSystem(maxPlayers);
+            InstallAudioSystem(maxPlayers);
 
-//            InstallLoopSystem<TGameState>();
-//        }
+            InstallLoopSystem<TGameState>();
+        }
 
-//        private static void InstallLoopSystem<TGameState>()
-//        {
-//            LoopSystem<TGameState>.serialize = (BinaryWriter bw, ref TGameState gameState, object userData) =>
-//            {
-//                var serializeContext = new SerializeContext(bw, false, (DefinitionObjectTable) userData);
-//                Field.Serialize(serializeContext, bw, ref gameState);
-//            };
-//            LoopSystem<TGameState>.deserialize = (BinaryReader br, ref TGameState gameState, object userData) =>
-//            {
-//                var deserializeContext = new DeserializeContext(br, (DefinitionObjectTable) userData);
-//                Field.Deserialize(deserializeContext, br, ref gameState);
-//            };
-//        }
+        private static void InstallLoopSystem<TGameState>()
+        {
+            LoopSystem<TGameState>.serialize = (BinaryWriter bw, ref TGameState gameState, object userData) =>
+            {
+                var serializeContext = new SerializeContext(bw, false, (DefinitionObjectTable) userData);
+                Field.Serialize(serializeContext, bw, ref gameState);
+            };
+            LoopSystem<TGameState>.deserialize = (BinaryReader br, ref TGameState gameState, object userData) =>
+            {
+                var deserializeContext = new DeserializeContext(br, (DefinitionObjectTable) userData);
+                Field.Deserialize(deserializeContext, br, ref gameState);
+            };
+        }
 
-//        private static void InstallAudioSystem(int maxPlayers)
-//        {
-//            AudioSystem.worldToAudio = (owner, x, y, z) =>
-//            {
-//                var camera = (Camera) owner;
-//                var position = camera.WorldToAudio(new Position(x, y, z));
-//                return new PitchPan(position.X, position.Y);
-//            };
+        private static void InstallAudioSystem(int maxPlayers)
+        {
+            AudioSystem.worldToAudio = (owner, x, y, z) =>
+            {
+                var camera = (Camera) owner;
+                var position = camera.WorldToAudio(new Position(x, y, z));
+                return new PitchPan(position.X, position.Y);
+            };
 
-//            AudioSystem.getMaxPlayers = () => maxPlayers;
-//            AudioSystem.audioDeviceCheck = AudioDeviceCheck;
-//            AudioSystem.getMasterVolume = owner => SoundEffect.MasterVolume;
-//            AudioSystem.setMasterVolume = (owner, value) => SoundEffect.MasterVolume = value;
+            AudioSystem.getMaxPlayers = () => maxPlayers;
+            AudioSystem.audioDeviceCheck = AudioDeviceCheck;
+            AudioSystem.getMasterVolume = owner => SoundEffect.MasterVolume;
+            AudioSystem.setMasterVolume = (owner, value) => SoundEffect.MasterVolume = value;
 
-//            AudioSystem.getPlayerAudioPosition = (owner, playerIndex) =>
-//            {
-//                var gameState = (IGameState) owner;
-//                var position = gameState.GetPlayerPosition(playerIndex);
-//                return position?.AsAudioPosition();
-//            };
+            AudioSystem.getPlayerAudioPosition = (owner, playerIndex) =>
+            {
+                var gameState = (IGameState) owner;
+                var position = gameState.GetPlayerPosition(playerIndex);
+                return position;
+            };
 
-//            //
-//            // SoundEffectInstance:
-//            {
-//                AudioSystem.createSoundEffectInstance = owner => ((SoundEffect) owner)?.CreateInstance();
+            //
+            // SoundEffectInstance:
+            {
+                AudioSystem.createSoundEffectInstance = owner => ((SoundEffect) owner).CreateInstance();
 
-//                AudioSystem.playSoundEffectInstance = owner => ((SoundEffectInstance) owner).Play();
-//                AudioSystem.stopSoundEffectInstance = owner => ((SoundEffectInstance) owner).Stop();
+                AudioSystem.playSoundEffectInstance = owner => ((SoundEffectInstance) owner).Play();
+                AudioSystem.stopSoundEffectInstance = owner => ((SoundEffectInstance) owner).Stop();
 
-//                AudioSystem.getIsLooped = owner => ((SoundEffectInstance) owner).IsLooped;
-//                AudioSystem.setIsLooped = (owner, value) => ((SoundEffectInstance) owner).IsLooped = value;
+                AudioSystem.getIsLooped = owner => ((SoundEffectInstance) owner).IsLooped;
+                AudioSystem.setIsLooped = (owner, value) => ((SoundEffectInstance) owner).IsLooped = value;
 
-//                AudioSystem.getVolume = owner => ((SoundEffectInstance) owner).Volume;
-//                AudioSystem.setVolume = (owner, value) => ((SoundEffectInstance) owner).Volume = value;
+                AudioSystem.getVolume = owner => ((SoundEffectInstance) owner).Volume;
+                AudioSystem.setVolume = (owner, value) => ((SoundEffectInstance) owner).Volume = value;
 
-//                AudioSystem.getPitch = owner => ((SoundEffectInstance) owner).Pitch;
-//                AudioSystem.setPitch = (owner, value) => ((SoundEffectInstance) owner).Pitch = value;
+                AudioSystem.getPitch = owner => ((SoundEffectInstance) owner).Pitch;
+                AudioSystem.setPitch = (owner, value) => ((SoundEffectInstance) owner).Pitch = value;
 
-//                AudioSystem.getPan = owner => ((SoundEffectInstance) owner).Pan;
-//                AudioSystem.setPan = (owner, value) => ((SoundEffectInstance) owner).Pan = value;
+                AudioSystem.getPan = owner => ((SoundEffectInstance) owner).Pan;
+                AudioSystem.setPan = (owner, value) => ((SoundEffectInstance) owner).Pan = value;
 
-//                AudioSystem.getSoundState = owner => (SoundState) ((SoundEffectInstance) owner).State;
+                AudioSystem.getSoundState = owner => (SoundState) ((SoundEffectInstance) owner).State;
 
-//                AudioSystem.getDuration = owner => ((SoundEffect) owner).Duration;
+                AudioSystem.getDuration = owner => ((SoundEffect) owner).Duration;
 
-//                AudioSystem.setFadePitchPan = (owner, fade, pitch, pan) =>
-//                {
-//                    owner.Volume = fade;
-//                    owner.Pitch = pitch;
-//                    owner.Pan = pan;
-//                };
-//            }
+                AudioSystem.setFadePitchPan = (owner, fade, pitch, pan) =>
+                {
+                    owner.Volume = fade;
+                    owner.Pitch = pitch;
+                    owner.Pan = pan;
+                };
+            }
 
-//            //
-//            // SoundEffect:
-//            AudioSystem.playSoundEffect = (owner, volume, pitch, pan) => ((SoundEffect) owner).Play(volume, pitch, pan);
-//            AudioSystem.createSoundEffectFromStream = stream => new SafeSoundEffect(SoundEffect.FromStream(stream));
-//            AudioSystem.createSoundEffectFromFile = path =>
-//            {
-//                using (var fs = File.OpenRead(path))
-//                {
-//                    return new SafeSoundEffect(SoundEffect.FromStream(fs));
-//                }
-//            };
+            //
+            // SoundEffect:
+            AudioSystem.playSoundEffect = (owner, volume, pitch, pan) => ((SoundEffect) owner).Play(volume, pitch, pan);
+            AudioSystem.createSoundEffectFromStream = stream => new SafeSoundEffect(SoundEffect.FromStream(stream));
+            AudioSystem.createSoundEffectFromFile = path =>
+            {
+                using (var fs = File.OpenRead(path))
+                {
+                    return new SafeSoundEffect(SoundEffect.FromStream(fs));
+                }
+            };
 
-//            AudioSystem.reportMissingCue = (name, debugContext) =>
-//            {
-//                string c;
-//                if (debugContext is string s)
-//                    c = s;
-//                else if (debugContext is IEditorNameProvider provider)
-//                    c = $"{provider.GetType().Name}: {provider.EditorName}";
-//                else if (debugContext != null)
-//                    c = debugContext.ToString();
-//                else
-//                    c = "[no context]";
+            AudioSystem.reportMissingCue = (name, debugContext) =>
+            {
+                string c;
+                var s = debugContext as string;
+                if (s != null)
+                    c = s;
+                else if (debugContext is IEditorNameProvider)
+                {
+                    var provider = (IEditorNameProvider)debugContext;
+                    c = string.Format("{0}: {1}", provider.GetType().Name, provider.EditorName);
+                }
+                else if (debugContext != null)
+                    c = debugContext.ToString();
+                else
+                    c = "[no context]";
 
-//                string message = $"Missing cue \"{name}\" (context: {c})";
-//                Debug.WriteLine(message);
-//                Log.Current.Warn(message);
-//            };
+                string message = string.Format("Missing cue \"{0}\" (context: {1})", name, c);
+                Debug.WriteLine(message);
+                Log.Current.Warn(message);
+            };
 
-//            AudioSystem.reportExpectedCue = (context, args) =>
-//            {
-//                AnimationSet animationSet = args[0] as AnimationSet;
-//                Animation animation = args[1] as Animation;
-//                int frame = (int) args[2];
+            AudioSystem.reportExpectedCue = (context, args) =>
+            {
+                AnimationSet animationSet = args[0] as AnimationSet;
+                Animation animation = args[1] as Animation;
+                int frame = (int) args[2];
 
-//                string format;
-//                if (animation == null)
-//                    format = "Expected cue for \"{0}\" on AnimationSet = {1}";
-//                else if (frame == -1)
-//                    format = "Expected cue for \"{0}\" on Animation = {2} (AnimationSet = {1})";
-//                else
-//                    format = "Expected cue for \"{0}\" on Frame = {3} (AnimationSet = {1}, Animation = {2})";
+                string format;
+                if (animation == null)
+                    format = "Expected cue for \"{0}\" on AnimationSet = {1}";
+                else if (frame == -1)
+                    format = "Expected cue for \"{0}\" on Animation = {2} (AnimationSet = {1})";
+                else
+                    format = "Expected cue for \"{0}\" on Frame = {3} (AnimationSet = {1}, Animation = {2})";
 
-//                string message = string.Format(format, context, animationSet == null ? "???" : animationSet.friendlyName,
-//                    animation == null ? "???" : animation.friendlyName, frame);
-//                Debug.WriteLine(message);
-//                Log.Current.Warn(message);
-//            };
-//        }
+                string message = string.Format(format, context, animationSet == null ? "???" : animationSet.friendlyName,
+                    animation == null ? "???" : animation.friendlyName, frame);
+                Debug.WriteLine(message);
+                Log.Current.Warn(message);
+            };
+        }
 
-//        private static bool AudioDeviceCheck()
-//        {
-//            try
-//            {
-//                SoundEffect.MasterVolume = 1f;
-//                // The above line should throw an exception if there is no audio device
-//                return true;
-//            }
-//            catch (NoAudioHardwareException)
-//            {
-//                Debug.WriteLine("No audio hardware available");
-//                return false;
-//            }
-//            catch (Exception e)
-//            {
-//                Debug.WriteLine("Exception during audio device testing. XNA or something under it doing something dumb.");
-//                Log.Current.Warn(e, "Exception during audio device testing");
-//                return false;
-//            }
-//        }
-//    }
-//}
+        private static bool AudioDeviceCheck()
+        {
+            try
+            {
+                SoundEffect.MasterVolume = 1f;
+                // The above line should throw an exception if there is no audio device
+                return true;
+            }
+            catch (NoAudioHardwareException)
+            {
+                Debug.WriteLine("No audio hardware available");
+                return false;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Exception during audio device testing. XNA or something under it doing something dumb.");
+                Log.Current.Warn(e, "Exception during audio device testing");
+                return false;
+            }
+        }
+    }
+}
