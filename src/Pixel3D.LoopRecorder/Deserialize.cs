80598ad971ea588cbd1aca9843518679ed234079
Daniel Crenna <daniel@hq.io>
2018-09-12 00:59:24 -0400

    fixup delegates

---
+++ src/Pixel3D.LoopRecorder/Deserialize.cs
@@ -0,0 +1,9 @@
+﻿// Copyright © Conatus Creative, Inc. All rights reserved.
+// Licensed under the Apache 2.0 License. See LICENSE.md in the project root for license terms.
+
+using System.IO;
+
+namespace Pixel3D.LoopRecorder
+{
+	public delegate void Deserialize<TGameState>(BinaryReader br, ref TGameState gameState, object userData);
+}
--- src/Pixel3D.LoopRecorder/Serialize.cs
+++ src/Pixel3D.LoopRecorder/Serialize.cs
@@ -6,6 +6,4 @@ using System.IO;
 namespace Pixel3D.LoopRecorder
 {
 	public delegate void Serialize<TGameState>(BinaryWriter bw, ref TGameState gameState, object userData);
-
-	public delegate void Deserialize<TGameState>(BinaryReader br, ref TGameState gameState, object userData);
 }
