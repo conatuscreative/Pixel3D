7994e3eb52df916f5fe18bac15ccdd770f229db3
Daniel Crenna <daniel@hq.io>
2018-09-16 23:40:01 -0400

    created introspection support class

--- src/Pixel3D.Editing.XNA/Properties/AssemblyInfo.cs
+++ src/Pixel3D.Editing.XNA/Properties/AssemblyInfo.cs
@@ -5,22 +5,24 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Pixel3D.Editor")]
+[assembly: AssemblyTitle("Pixel3D.Editing.XNA")]
+[assembly: AssemblyProduct("Pixel3D.Editing.XNA")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Pixel3D.Editor")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b73e54a8-2d7d-4c24-a8c3-cfd08bb027f6")]
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("d6c03668-1c25-405f-b6c8-61785017f6ae")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +31,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Pixel3D.Editing/Introspection.cs
@@ -0,0 +1,133 @@
+﻿// Copyright © Conatus Creative, Inc. All rights reserved.
+// Licensed under the Apache 2.0 License. See LICENSE.md in the project root for license terms.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Pixel3D.Editing
+{
+	public static class Introspection
+	{
+		private static Type[] _externalTypes;
+
+		private static readonly Dictionary<string, Assembly> VisitedAssemblies;
+		private static readonly HashSet<string> VisitedFilePaths;
+
+		static Introspection()
+		{
+			AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += CurrentDomainOnReflectionOnlyAssemblyResolve;
+			VisitedAssemblies = new Dictionary<string, Assembly>();
+			VisitedFilePaths = new HashSet<string>();
+		}
+
+		public static void Invalidate()
+		{
+			VisitedAssemblies.Clear();
+			VisitedFilePaths.Clear();
+			_externalTypes = null;
+		}
+
+		private static Assembly CurrentDomainOnReflectionOnlyAssemblyResolve(object sender, ResolveEventArgs args)
+		{
+			Assembly assembly;
+			if (VisitedAssemblies.TryGetValue(args.Name, out assembly))
+				return assembly;
+
+			var assemblyName = new AssemblyName(args.Name);
+			if (!IsValidArchitecture(assemblyName))
+				return null;
+
+			try
+			{
+				assembly = Assembly.ReflectionOnlyLoad(assemblyName.FullName);
+			}
+			catch (FileNotFoundException)
+			{
+				var baseDirectory = Path.GetDirectoryName(args.RequestingAssembly.Location);
+				if (baseDirectory == null)
+					throw new NullReferenceException("could not resolve requesting assembly's directory");
+
+				var guessFileName = Path.Combine(baseDirectory, assemblyName.Name + ".dll");
+				if (File.Exists(guessFileName))
+					assembly = Assembly.ReflectionOnlyLoadFrom(guessFileName);
+				else
+					foreach (var assemblyFile in Directory.GetFiles(baseDirectory))
+						if (AssemblyName.GetAssemblyName(assemblyFile) == assemblyName)
+							assembly = Assembly.ReflectionOnlyLoadFrom(assemblyFile);
+
+				if (assembly == null)
+					throw new InvalidOperationException(
+						"unable to resolve a referenced assembly in the introspection folder");
+			}
+
+			VisitedAssemblies.Add(assembly.FullName, assembly);
+			VisitedFilePaths.Add(assembly.Location);
+			return assembly;
+		}
+
+		public static IEnumerable<Type> GetExportedTypes(string introspectionDir,
+			Func<Assembly, bool> assemblyFilterFunc)
+		{
+			if (_externalTypes == null)
+				if (!string.IsNullOrWhiteSpace(introspectionDir))
+				{
+					var list = new HashSet<Type>();
+					foreach (var filePath in Directory.GetFiles(introspectionDir, "*.dll", SearchOption.AllDirectories))
+						try
+						{
+							if (VisitedFilePaths.Contains(filePath))
+								continue;
+
+							var assembly = Assembly.ReflectionOnlyLoadFrom(filePath);
+							VisitedAssemblies.Add(assembly.FullName, assembly);
+
+							var assemblyName = assembly.GetName();
+							if (!IsValidArchitecture(assemblyName))
+								continue;
+
+							if (!assemblyFilterFunc(assembly))
+								continue;
+
+							var types = assembly.GetExportedTypes();
+							foreach (var type in types)
+								list.Add(type);
+						}
+						catch (Exception e)
+						{
+							Trace.TraceError(e.ToString());
+						}
+						finally
+						{
+							VisitedFilePaths.Add(filePath);
+						}
+
+					_externalTypes = list.ToArray();
+				}
+
+			return _externalTypes;
+		}
+
+		private static bool IsValidArchitecture(AssemblyName assemblyName)
+		{
+			var isx64 = Environment.Is64BitProcess;
+			switch (assemblyName.ProcessorArchitecture)
+			{
+				case ProcessorArchitecture.X86:
+					if (isx64)
+						return false;
+					break;
+				case ProcessorArchitecture.IA64:
+				case ProcessorArchitecture.Amd64:
+					if (!isx64)
+						return false;
+					break;
+			}
+
+			return true;
+		}
+	}
+}
