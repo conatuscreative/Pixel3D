745a782c0239c82f7426883c984ba92b240519ae
Daniel Crenna <daniel@hq.io>
2018-09-25 15:09:46 -0400

    move PlatformSettings to P3D

---
+++ src/Pixel3D.Engine.FNA/PlatformSettings.cs
@@ -0,0 +1,62 @@
+﻿// Copyright © Conatus Creative, Inc. All rights reserved.
+// Licensed under the Apache 2.0 License. See LICENSE.md in the project root for license terms.
+
+using System;
+using System.IO;
+using SDL2;
+
+namespace Pixel3D.Engine.FNA
+{
+	public static class PlatformSettings
+	{
+		public static string GetPlatformSettingsDir()
+		{
+			string os = SDL.SDL_GetPlatform();
+			if (os.Equals("Linux"))
+			{
+				string osDir = Environment.GetEnvironmentVariable("XDG_CONFIG_HOME");
+				if (string.IsNullOrEmpty(osDir))
+				{
+					osDir = Environment.GetEnvironmentVariable("HOME");
+					if (string.IsNullOrEmpty(osDir))
+					{
+						return @"."; // Oh well.
+					}
+
+					return CreateAndReturnDir(Path.Combine(osDir, ".config/RiverCityRansomUnderground"));
+				}
+
+				return CreateAndReturnDir(Path.Combine(osDir, "RiverCityRansomUnderground"));
+			}
+
+			if (os.Equals("Mac OS X"))
+			{
+				string osDir = Environment.GetEnvironmentVariable("HOME");
+				if (string.IsNullOrEmpty(osDir))
+				{
+					return @"."; // Oh well.
+				}
+
+				return CreateAndReturnDir(Path.Combine(osDir,
+					"Library/Application Support/RiverCityRansomUnderground"));
+			}
+
+			if (!os.Equals("Windows"))
+			{
+				throw new NotSupportedException("Unhandled SDL2 platform!");
+			}
+
+			return @".";
+		}
+
+		public static string CreateAndReturnDir(string dir)
+		{
+			if (!Directory.Exists(dir))
+			{
+				Directory.CreateDirectory(dir);
+			}
+
+			return dir;
+		}
+	}
+}
