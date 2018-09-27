e56de5fda8d1b26d63086a1d5fd8781b93e5ec15
Daniel Crenna <daniel@hq.io>
2018-09-27 00:06:41 -0400

    cleanup

--- src/Pixel3D.Serialization.Support/CustomFieldSerialization.cs
+++ src/Pixel3D.Serialization.Support/CustomFieldSerialization.cs
@@ -17,6 +17,58 @@ namespace Pixel3D
 {
 	public static partial class CustomFieldSerialization
 	{
+		#region OrderedDictionary
+
+		public static void SerializeOrderedDictionary<T>(this OrderedDictionary<string, T> dictionary, AnimationSerializeContext context, Action<T> serializeValue)
+		{
+			context.bw.WriteSmallInt32(dictionary.Count);
+
+			foreach (var item in dictionary)
+			{
+				var key = item.Key;
+				var value = item.Value;
+				context.bw.Write(key);
+				serializeValue(value);
+			}
+		}
+
+		public static OrderedDictionary<string, T> DeserializeOrderedDictionary<T>(this AnimationDeserializeContext context, Func<T> deserializeValue)
+		{
+			var dictionary = new OrderedDictionary<string, T>();
+
+			int count = context.br.ReadSmallInt32();
+
+			for (var i = 0; i < count; i++)
+			{
+				var key = context.br.ReadString();
+				var value = deserializeValue();
+				dictionary.Add(key, value);
+			}
+
+			return dictionary;
+		}
+
+		#endregion
+
+		#region TagLookup
+
+		// NOTE: Pass-through the animation serializer to a simple binary serializer (the format of `TagLookup` is *really* stable, and some folks need to directly serialize us)
+
+		public static void SerializeTagLookup<T>(this TagLookup<T> tagLookup, AnimationSerializeContext context,
+			Action<T> serializeValue)
+		{
+			tagLookup.Serialize(context.bw, serializeValue);
+		}
+
+		/// <summary>Deserialize into new object instance</summary>
+		public static TagLookup<T> DeserializeTagLookup<T>(this AnimationDeserializeContext context,
+			Func<T> deserializeValue)
+		{
+			return new TagLookup<T>(context.br, deserializeValue);
+		}
+
+		#endregion
+
 		#region TagLookup
 
 		// Due to a limitation in the serializer generator, this needs to be in a different class to TagLookup<T>
@@ -55,41 +107,6 @@ namespace Pixel3D
 
 		#endregion
 
-		#region TagSet
-
-		// NOTE: Pass-through the animation serializer to a simple binary serializer (the format of `TagSet` is *really* stable, and some folks need to directly serialize us)
-
-		public static void SerializeTagSet(this TagSet tagSet, AnimationSerializeContext context)
-		{
-			tagSet.Serialize(context.bw);
-		}
-
-		public static TagSet DeserializeTagSet(this AnimationDeserializeContext context)
-		{
-			return new TagSet(context.br);
-		}
-
-		#endregion
-
-		#region TagLookup
-
-		// NOTE: Pass-through the animation serializer to a simple binary serializer (the format of `TagLookup` is *really* stable, and some folks need to directly serialize us)
-
-		public static void SerializeTagLookup<T>(this TagLookup<T> tagLookup, AnimationSerializeContext context,
-			Action<T> serializeValue)
-		{
-			tagLookup.Serialize(context.bw, serializeValue);
-		}
-
-		/// <summary>Deserialize into new object instance</summary>
-		public static TagLookup<T> DeserializeTagLookup<T>(this AnimationDeserializeContext context,
-			Func<T> deserializeValue)
-		{
-			return new TagLookup<T>(context.br, deserializeValue);
-		}
-
-		#endregion
-
 		#region ImageBundle
 
 		[CustomFieldSerializer]
@@ -118,72 +135,6 @@ namespace Pixel3D
 		public static void Deserialize(DeserializeContext context, BinaryReader br, ref ImageBundleManager value)
 		{
 			throw new InvalidOperationException();
-		}
-
-		#endregion
-
-		#region SpriteRef
-
-		// Definition-only (TODO: Maybe we should care that the sprite references match up between players?)
-		[CustomSerializer]
-		public static void Serialize(SerializeContext context, BinaryWriter bw, ref SpriteRef value) { }
-
-		[CustomSerializer]
-		public static void Deserialize(DeserializeContext context, BinaryReader br, ref SpriteRef value)
-		{
-			throw new InvalidOperationException();
-		}
-		
-		#endregion
-
-		#region Sprite
-
-		[CustomSerializer]
-		public static void Serialize(SerializeContext context, BinaryWriter bw, ref Sprite value)
-		{
-			// NOTE: Not visiting the texture object, because it could be deferred (so definitions can't know about it)
-		}
-
-		[CustomSerializer]
-		public static void Deserialize(DeserializeContext context, BinaryReader br, ref Sprite value)
-		{
-			Debug.Assert(false); // Shouldn't happen! (Can't store Sprite in game state)
-			throw new InvalidOperationException();
-		}
-
-		#endregion
-
-		#region OrderedDictionary
-
-		public static void SerializeOrderedDictionary<T>(this OrderedDictionary<string, T> dictionary,
-			AnimationSerializeContext context, Action<T> serializeValue)
-		{
-			context.bw.WriteSmallInt32(dictionary.Count);
-
-			foreach (var item in dictionary)
-			{
-				var key = item.Key;
-				var value = item.Value;
-				context.bw.Write(key);
-				serializeValue(value);
-			}
-		}
-
-		public static OrderedDictionary<string, T> DeserializeOrderedDictionary<T>(
-			this AnimationDeserializeContext context, Func<T> deserializeValue)
-		{
-			var dictionary = new OrderedDictionary<string, T>();
-
-			int count = context.br.ReadSmallInt32();
-
-			for (var i = 0; i < count; i++)
-			{
-				var key = context.br.ReadString();
-				var value = deserializeValue();
-				dictionary.Add(key, value);
-			}
-
-			return dictionary;
 		}
 
 		#endregion
---
+++ src/Pixel3D.Serialization.Support/CustomSerialization.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using Pixel3D.Serialization;
+using Pixel3D.Serialization.Context;
+
+namespace Pixel3D
+{
+	public static class CustomSerialization
+	{
+		#region SpriteRef
+
+		// Definition-only (TODO: Maybe we should care that the sprite references match up between players?)
+		[CustomSerializer]
+		public static void Serialize(SerializeContext context, BinaryWriter bw, ref SpriteRef value) { }
+
+		[CustomSerializer]
+		public static void Deserialize(DeserializeContext context, BinaryReader br, ref SpriteRef value)
+		{
+			throw new InvalidOperationException();
+		}
+
+		#endregion
+
+		#region Sprite
+
+		[CustomSerializer]
+		public static void Serialize(SerializeContext context, BinaryWriter bw, ref Sprite value)
+		{
+			// NOTE: Not visiting the texture object, because it could be deferred (so definitions can't know about it)
+		}
+
+		[CustomSerializer]
+		public static void Deserialize(DeserializeContext context, BinaryReader br, ref Sprite value)
+		{
+			Debug.Assert(false); // Shouldn't happen! (Can't store Sprite in game state)
+			throw new InvalidOperationException();
+		}
+
+		#endregion
+	}
+}
