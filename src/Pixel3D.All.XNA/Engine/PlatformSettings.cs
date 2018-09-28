43079ca133ad2562c3cdc3c07452f588e520f7ce
Daniel Crenna <daniel@hq.io>
2018-09-28 01:26:04 -0400

    fixup XNA legacy build

---
+++ src/Pixel3D.All.XNA/Engine/PlatformSettings.cs
@@ -0,0 +1,31 @@
+﻿// Copyright © Conatus Creative, Inc. All rights reserved.
+// Licensed under the Apache 2.0 License. See LICENSE.md in the project root for license terms.
+
+using System;
+using System.IO;
+
+namespace Pixel3D.Engine
+{
+	public static class PlatformSettings
+	{
+		public static string GetPlatformSettingsDir(string gameTitlePath)
+		{
+			return @".";
+		}
+
+		public static string CreateAndReturnDir(string dir)
+		{
+			if (!Directory.Exists(dir))
+			{
+				Directory.CreateDirectory(dir);
+			}
+
+			return dir;
+		}
+
+		public static string GetLoggingDir()
+		{
+			return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Conatus Creative", "RCRU");
+		}
+	}
+}
