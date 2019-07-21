f6c1085f223306c7d24ed48f34a16851f517550b
Daniel Crenna <daniel.crenna@gmail.com>
2019-07-21 01:54:34 -0400

    add level effect interface

---
+++ src/Pixel3D.Levels/ILevelEffect.cs
@@ -0,0 +1,15 @@
+﻿// Copyright © Conatus Creative, Inc. All rights reserved.
+// Licensed under the Apache 2.0 License. See LICENSE.md in the project root for license terms.
+
+using CRTSim;
+using Microsoft.Xna.Framework.Graphics;
+using Pixel3D.ActorManagement;
+
+namespace Pixel3D.Levels
+{
+    public interface ILevelEffect
+    {
+        void LevelEffect(GameState gameState, RenderTarget2D inputRenderTarget, SpriteBatch sb, FadeEffect fadeEffect,
+            int fadeLevel, bool disabled);
+    }
+}
